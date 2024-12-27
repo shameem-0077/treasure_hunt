2536e651fd474479f04964645b7c09e77b745b48
shameem-muhammed <shameem.talrop@gmail.com>
2024-12-27 21:18:12 +0530

    Add static and media file configurations in settings and update URL patterns

--- treasure_hunt/urls.py
+++ treasure_hunt/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -32,4 +35,7 @@ urlpatterns = [
     path('api/v1/users/', include('users.urls', namespace="api_v1_users")),
     # path('api/v1/core/', include('core.urls', namespace="api_v1_core")),
     path('api/v1/hunts/', include('hunts.urls', namespace="api_v1_hunts")),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
